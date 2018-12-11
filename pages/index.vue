d88c21997a36ebca12163dd90a82e65b3ccf7aaf
mikakane <t.goto@chatbox-inc.com>
2018-12-11 11:15:57 +0900

    update

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        laracon
+      </h1>
+      <h2 class="subtitle">
+        My pioneering Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+
+</style>
