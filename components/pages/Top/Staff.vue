8246baf5ec46863ea9e231a588c22600c3a067e4
yasui05821 <yasui05821@gmail.com>
2018-12-13 18:11:12 +0900

    staff　修正

--- components/pages/Top/Staff.vue
+++ components/pages/Top/Staff.vue
@@ -1,269 +1,221 @@
 <template>
-  <div class="p-staff">
-    <div class="parent">
-      <img src="~assets/images/staff/staff_left.png" class="c-left">
-      <img src="~assets/images/staff/staff_right.png" class="c-right">
+  <section class="p-staff">
+    <div class="bg_deco">
+      <img src="~assets/images/staff/staff_left.png" class="bg_cloudLerf">
+      <img src="~assets/images/staff/staff_right.png" class="bg_cloudRight">
     </div>
     <div class="c-container">
-      <div class="parent">
-        <img src="~assets/images/staff/staff_title.png" class="c-stafftitle">
-      </div>
-
-      <div class="title">
-        STAFF
-      </div>
-
-<div class="box">
-
-
-      <div class="item">
-
-        <div class="grid">
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-          <div class="staff">
-            <div class="staff-box">
-              <div class="img"></div>
-              <p class="c-name">
-                田中 太郎
-              </p>
-            </div>
-          </div>
-
-
+      <h1 class="title">
+        Staff
+      </h1>
+      <span class="subTitle">STAFF</span>
+      <div class="staffList">
+        <div class="staffList_item">
+          <img src="https://via.placeholder.com/350x150" alt="" class="staffList_icon">
+          <span class="staffList_name">
+            hodhiaohdskalfjhlsj
+          </span>
         </div>
-
-
+        <div class="staffList_item">
+          <img src="https://via.placeholder.com/350x150" alt="" class="staffList_icon">
+          <span class="staffList_name">
+            田中 太郎
+          </span>
+        </div>
+        <div class="staffList_item">
+          <img src="https://via.placeholder.com/350x150" alt="" class="staffList_icon">
+          <span class="staffList_name">
+            田中 太郎
+          </span>
+        </div>
+        <div class="staffList_item">
+          <img src="https://via.placeholder.com/350x150" alt="" class="staffList_icon">
+          <span class="staffList_name">
+            田中 太郎
+          </span>
+        </div>
+        <div class="staffList_item">
+          <img src="https://via.placeholder.com/350x150" alt="" class="staffList_icon">
+          <span class="staffList_name">
+            田中 太郎
+          </span>
+        </div>
+        <div class="staffList_item">
+          <img src="https://via.placeholder.com/350x150" alt="" class="staffList_icon">
+          <span class="staffList_name">
+            田中 太郎
+          </span>
+        </div>
+        <div class="staffList_item">
+          <img src="https://via.placeholder.com/350x150" alt="" class="staffList_icon">
+          <span class="staffList_name">
+            田中 太郎
+          </span>
+        </div>
+        <div class="staffList_item">
+          <img src="https://via.placeholder.com/350x150" alt="" class="staffList_icon">
+          <span class="staffList_name">
+            田中 太郎
+          </span>
+        </div>
       </div>
-
     </div>
-    </div>
-  </div>
+  </section>
 </template>
 
 <script>
 </script>
 
 <style lang="scss" scoped>
-  @import "~/assets/scss/library/variable.scss";
-  @import "~/assets/scss/library/mixin.scss";
+  @import "~/assets/scss/common.scss";
 
-  .title {
-    color: #64547B;
-    font-size:4rem;
-    font-family: playfair-display; //ここが適用されない
-    font-weight: bold;
-    text-align:left;
-    margin-bottom:3.4rem;
-    position: relative
+  .p-staff {
+    @include space_section;
+    padding-bottom: 100px;
+    .bg_deco {
+      margin: 0 auto;
+      width: 100%;
+      position: relative;
+
+      @include media_desktop {
+        max-width: 1500px;
+
+      }
+    }
+    .bg_cloudLerf{
+      width: 20vw;
+      min-width: 300px;
+      max-width: 500px;
+      position: absolute;
+      left: 0;
+    }
+    .bg_cloudRight {
+      width: 20vw;
+      min-width: 300px;
+      max-width: 500px;
+      position: absolute;
+      bottom: -350px;
+      right: 0;
+      @include media_desktop {
+        bottom: -200px;
+      }
+    }
+
+
+    .c-container {
+      position: relative;
+    }
+
+
+    .title {
+      @include typo_sectionTitleEn
+    }
+    .subTitle {
+      @include typo_sectionSubTitleEn;
+    }
+
+    .staffList {
+      max-width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: flex-start;
+      @include media_desktop{
+        max-width: 90%;
+      }
+    }
+    .staffList_item {
+      margin: 0 10px;
+      margin-bottom: 20px;
+      @include media_desktop {
+        margin: 0 15px;
+        margin-bottom: 30px;
+      }
+    }
+    .staffList_icon {
+      margin-bottom: 5px;
+      display: block;
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+      overflow: hidden;
+      background: image;
+      @include media_desktop {
+        width: 70px;
+        height: 70px;
+      }
+
+    }
+    .staffList_name {
+      display: block;
+      max-width: 60px;
+      font-size: 1.3rem;
+      line-height: 1.4;
+      word-break: break-all;
+      text-align: center;
+      color: $clr_fontBase;
+      @include media_desktop {
+        font-size: 1.4rem;
+        max-width: 70px;
+      }
+    }
+
   }
 
-  .staff-box{
-    max-width:150px;
-    text-align:center;
+
+  .staff-box {
+    max-width: 150px;
+    text-align: center;
   }
 
-
-  .img{
-    width:70px;
-    height:70px;
+  .img {
+    width: 70px;
+    height: 70px;
     background-color: #eee;
-    border-radius:50%;
-    display:inline-block;
+    border-radius: 50%;
+    display: inline-block;
   }
 
-  .c-name{
+  .c-name {
     color: #64547B;
     font-size: 1.6rem;
     display: inline-block;
-    text-align:center;
+    text-align: center;
   }
 
   .grid {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     justify-items: center;
-    @include desktop {
+    @include media_desktop {
       grid-template-columns: repeat(6, 1fr);
 
     }
   }
 
-  .item{
+  .item {
     grid-area: item;
     display: flex;
-    justify-content:  start;
+    justify-content: start;
 
   }
-  .box{
+
+  .box {
     display: flex;
     position: relative;
-    z-index:999;
+    z-index: 999;
   }
 
   .c-stafftitle {
     position: absolute;
     max-height: 390px;
     top: -70px;
-    right:0px;
+    right: 0px;
     opacity: 0;
-    @include desktop{
+    @include media_desktop {
       position: absolute;
       max-height: 390px;
       top: -70px;
-      right:0px;
+      right: 0px;
       opacity: 100;
     }
 
@@ -273,22 +225,22 @@
     position: absolute;
     max-width: 390px;
     top: 100px;
-    left:0px;
+    left: 0px;
     max-width: 100%;
-    z-index:99;
+    z-index: 99;
   }
 
   .c-right {
     position: absolute;
     max-width: 470px;
     top: 100px;
-    right:0px;
-    z-index:99;
+    right: 0px;
+    z-index: 99;
   }
 
-  .parent{
+  .parent {
     position: relative;
-    z-index:99;
+    z-index: 99;
   }
 </style>
 
