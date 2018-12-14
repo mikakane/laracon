245b55777d36db2824f087b9d56e036c7420d5b2
yasui05821 <yasui05821@gmail.com>
2018-12-14 17:10:10 +0900

    fontawesome　読み込み

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,13 +12,12 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
-    ],
+      ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      //googlefont
-      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Lobster' }
-    ]
-  },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css', integrity: 'sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP', crossorigin:'anonymous'}
+      ]
+    },
 
   /*
   ** Customize the progress-bar color
