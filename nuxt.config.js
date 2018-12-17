0d1f1bef794dcda367cb517ac8dfe5ff71440b43
yasui05821 <yasui05821@gmail.com>
2018-12-17 17:45:11 +0900

    共通コード

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
+      { charset: 'UTF-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
       ],
