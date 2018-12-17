62e9f22ed74d476c0a2cda6725369de412c4373f
yasui05821 <yasui05821@gmail.com>
2018-12-17 17:45:46 +0900

    hero 途中

--- components/pages/Top/Hero.vue
+++ components/pages/Top/Hero.vue
@@ -1,14 +1,16 @@
 <template>
-<div class="l-hero">
-  <div class="c-container">
-    <div class="img">
-    <img src="~assets/images/hero/hero_logo.svg" class="p_herologo">
-    </div>
-    <div class="l-hero_text">
-      <p>2019</p>
-      <h2><span class="day">2</span>月<span class="day">16</span>日 <span class="hi">(土)</span>  10:00-</h2>
-      <h3>グランパークカンファレンス</h3>
-      <h4>#laraveljpcon</h4>
+<div class="p-hero">
+  <div class="bg_deco">
+    <div class="c-container">
+      <div class="img">
+      <img src="~assets/images/hero/hero_logo.svg" class="p_herologo">
+      </div>
+      <div class="l-hero_text">
+        <p>2019</p>
+        <h2><span class="day">2</span>月<span class="day">16</span>日 <span class="hi">(土)</span>  10:00-</h2>
+        <h3>グランパークカンファレンス</h3>
+        <h4>#laraveljpcon</h4>
+      </div>
     </div>
   </div>
 </div>
@@ -19,14 +21,21 @@
 </script>
 
 <style lang="scss" scoped>
-  @import "~/assets/scss/library/variable.scss";
-  @import "~/assets/scss/library/mixin.scss";
-
+  @import "~/assets/scss/common.scss";
 
 .l-hero {
+  background: #f27065;
+  background: -moz-linear-gradient(top, #f27065 0%, #a08383 100%);
+  background: -webkit-linear-gradient(top, #f27065 0%,#a08383 100%);
+  background: linear-gradient(to bottom, #f27065 0%,#a08383 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f27065', endColorstr='#a08383',GradientType=0 );
 
-  background-image: url("~assets/images/hero/hero_fuji.svg"),url( "~assets/images/hero/hero_cloud.svg"), url("~assets/images/hero/hero_bg.svg");
-  background-size: cover;
+  .bg_deco {
+    width: 100%;
+    min-height: 800px;
+    background-image: url("~assets/images/hero/hero_fuji.svg"),url( "~assets/images/hero/hero_cloud.svg"), url("~assets/images/hero/hero_bg.svg");
+    background-size: cover;
+  }
 
   &_text {
     color: #FFF9ED;
