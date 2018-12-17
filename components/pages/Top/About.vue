5cbc531314e7a45b95e94fdcfef5ff753ff224ce
yasui05821 <yasui05821@gmail.com>
2018-12-17 17:20:07 +0900

    about　PC/SP

--- components/pages/Top/About.vue
+++ components/pages/Top/About.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="l-about">
-    <div class="parent">
-      <img src="~assets/images/about/about_left.png" class="c-logo1">
-      <img src="~assets/images/about/about_right.png" class="c-logo2">
-    </div>
-    <div class="c-container">
-      <div class="parent">
-      <img src="~assets/images/about/about_title.png" class="c-abouttitle">
-      </div>
-    <div class="title">
-      Laracl JP conferene
-    </div>
-      <div class="text">
-        <p class="text">
+  <div class="p-about">
+    <div class="bg_deco">
+      <img src="~assets/images/about/about_left.png" class="bg_cloudLerf">
+      <img src="~assets/images/about/about_right.png" class="bg_cloudRight">
+      <div class="c-container">
+        <h2 class="title">
+          Laravel JP conferene
+        </h2>
+        <span class="subTitle">ABOUT</span>
+        <p class="description">
           Laravel JP Conferenceは PHPのフレームワークであるLaravelをメインテーマとした技術カンファレンスです。「Laravel JP」を冠したイベントとしては日本初開催となります。<br><br>
           <br>
           Laravelとは<br>
@@ -20,10 +16,15 @@
           <br>
           本イベントでは主に一般公募から参加していただくスピーカーによるトークセッションと、スポンサー様から参加していただくスピーカーのトークセッション他、懇親会や参加者同士で意見交換などができる場の提供を予定しています。<br><br>
         </p>
+        <div class="parent">
+        </div>
+        <div class="text">
+
+        </div>
+      <div class="box">
       </div>
-    <div class="box">
     </div>
-  </div>
+    </div>
   </div>
 </template>
 
@@ -31,57 +32,78 @@
 </script>
 
 <style lang="scss" scoped>
-  @import "~/assets/scss/library/variable.scss";
-  @import "~/assets/scss/library/mixin.scss";
+  @import "~/assets/scss/common.scss";
 
+  .p-about {
+    @include space_section;
 
-
-  .l-about{
     background-color: #FFFCF5;
-  }
 
-  .title {
-    color: #64547B;
-    font-size:4rem;
-    font-family: playfair-display;
-    font-weight: bold;
-    text-align:left;
-    padding-top:7rem;
-    padding-bottom:5rem;
-    position: relative;
-    z-index:99999;
+    .bg_deco {
+      margin: 0 auto;
+      width: 100%;
+      position: relative;
+
+      @include media_desktop {
+        max-width: 2500px;
+      }
+    }
+
+    .bg_cloudLerf{
+      width: 20vw;
+      min-width: 300px;
+      max-width: 500px;
+      position: absolute;
+      left: 0;
+      opacity: .9;
+      @include media_desktop {
+        opacity: 1;
+      }
+
+    }
+
+    .bg_cloudRight {
+      width: 20vw;
+      min-width: 300px;
+      max-width: 500px;
+      position: absolute;
+      bottom: 50px;
+      right: 0;
+      opacity: .9;
+
+      @include media_desktop {
+        bottom: -20px;
+        opacity: 1;
+      }
+    }
+
+    .c-container {
+      position: relative;
+    }
+
+    .title {
+      @include typo_sectionTitleEn
+    }
+    .subTitle {
+      @include typo_sectionSubTitleEn
+      top: -10rem;
+    }
+
+    .description {
+      max-width: 750px;
+      text-align: left;
+      font-size: 1.8rem;
+      color: $clr_fontBase;
+      position: relative;
+      z-index: 10;
+    }
   }
 
   .text {
-    color: #64547B;
-    text-align: left;
-    max-width:750px;
-    font-size: 1.7rem;
-    padding-bottom: 5rem;
-    position: relative;
-    z-index:99999;
+
   }
 
-  .c-logo1 {
-    position: absolute;
-    max-width: 640px;
-    top: 100px;
-    left:0px;
-    max-width: 100%;
-    @include desktop{
-    position: absolute;
-    max-width: 640px;
-    top: 100px;
-    left:0px;
-    }
-    }
 
-  .c-logo2 {
-    position: absolute;
-    max-width: 640px;
-    top: 350px;
-    right:0px;
-  }
 
   .c-abouttitle {
     position: absolute;
@@ -89,7 +111,7 @@
     top: -70px;
     right:0px;
     opacity: 0;
-    @include desktop{
+    @include media_desktop{
       position: absolute;
       max-height: 390px;
       top: -70px;
